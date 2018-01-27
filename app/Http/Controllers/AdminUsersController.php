2dcb457105dfdac0c66a805f2677bee9d0df6747
root <root@MOAMER.localdomain>
2018-01-27 14:53:36 +0100

    Persisting user with photo

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\User;
 use App\Role;
 use Illuminate\Http\Request;
@@ -42,10 +43,16 @@ class AdminUsersController extends Controller
     {
         // return $request->all();
 
-        // $input = $request->all();
-        if($request->file('photo_id')) {
-            return "photo exist";
+        $input = $request->all();
+        if($file = $request->file('photo_id')) {
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['file'=>$name]);
+            $input['photo_id'] = $photo->id;
         }
+
+        $input['password'] = bcrypt($request->password);
+        User::create($input);
 
         // return redirect('/admin/users');
     }
