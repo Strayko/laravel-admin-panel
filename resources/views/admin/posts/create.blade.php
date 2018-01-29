c87a014afe13c8da5b60a52fe2be20b0375be235
Moamer <stryko_lol@windowslive.com>
2018-01-29 15:04:25 +0100

    Displaying post and relationship setup

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+    <h1>CREATE POSTS</h1>
+
+@endsection
---
+++ resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+    <h1>EDIT POSTS</h1>
+
+@endsection
