00dfb275b67c4ba3c23ecebcc12b1be1a72ab6c5
Moamer <stryko_lol@windowslive.com>
2018-01-29 17:25:05 +0100

    Displaying images in post

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostsCreateRequest;
+use Illuminate\Support\Facades\Auth;
 use App\Post;
+use App\Photo;
 class AdminPostsController extends Controller
 {
     /**
@@ -34,9 +37,19 @@ class AdminPostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostsCreateRequest $request)
     {
-        //
+        $input = $request->all();
+        $user = Auth::user();
+        if($file = $request->file('photo_id')) {
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['file'=>$name]);
+            $input['photo_id'] = $photo->id;
+        }
+
+        $user->posts()->create($input);
+        return redirect('/admin/posts');
     }
 
     /**
