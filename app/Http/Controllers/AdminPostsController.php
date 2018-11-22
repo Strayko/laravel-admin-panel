bf1280951d67f36680a9ec1c4c98577bde25e8e0
Strayko <strayko_lol@windowslive.com>
2018-11-22 17:25:44 +0100

    editing part2 - lets edit the post

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -90,7 +90,17 @@ class AdminPostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $input = $request->all();
+
+        if($file = $request->file('photo_id')) {
+            $name = time(). $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::crete(['file'=>$name]);
+            $input['photo_id'] = $photo-id;
+        }
+        Auth::user()->posts()->whereId($id)->first()->update($input);
+        return redirect('/admin/posts');
+
     }
 
     /**
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -25,7 +25,7 @@
             <td><img height="50" src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400'}}" alt="" class="img-rounded"></td>
             <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
             <td>{{$post->title}}</td>
-            <td>{{$post->body}}</td>
+            <td>{{str_limit($post->body, 10)}}</td>
             <td>{{$post->created_at->diffForHumans()}}</td>
             <td>{{$post->updated_at->diffForHumans()}}</td>
           </tr>
