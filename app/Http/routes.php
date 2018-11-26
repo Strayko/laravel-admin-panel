b049f48c4e9f9bc2fef9789f555ab13cc762eb07
Strayko <strayko_lol@windowslive.com>
2018-11-26 13:36:20 +0100

    setting up and displaying media

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -41,6 +41,8 @@ Route::group(['middleware'=>['web','admin']], function() {
     Route::resource('/admin/users', 'AdminUsersController');
     Route::resource('/admin/posts', 'AdminPostsController');
     Route::resource('/admin/categories', 'AdminCategoriesController');
+    Route::resource('/admin/media', 'AdminMediasController');
+    Route::get('admin/media/upload',['as'=>'admin.media.upload', 'uses'=>'AdminMediasController@store']);
 
     Route::get('/admin', function() {
         return view('admin.index');
