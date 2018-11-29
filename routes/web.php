d319af1ed2355558314c82788643c2ad33b7a0c7
Strayko <strayko_lol@windowslive.com>
2018-11-29 17:33:15 +0100

    upgrading routes part 1

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,31 @@
 |
 */
 
-Route::get('/', function () {
-    return "work";
+
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/logout', 'Auth\LoginController@logout');
+    Route::auth();
+    Route::get('/home', 'HomeController@index');
+
+
+    //Admin routes
+
+});
+
+Route::group(['middleware'=>['web','admin']], function() {
+    Route::resource('/admin/users', 'AdminUsersController');
+    Route::resource('/admin/posts', 'AdminPostsController');
+    Route::resource('/admin/categories', 'AdminCategoriesController');
+    Route::resource('/admin/media', 'AdminMediasController');
+    Route::resource('/admin/comments', 'PostCommentsController');
+    Route::resource('/admin/comment/replies', 'CommentRepliesController');
+
+    Route::get('/post/{id}', ['as'=>'home.post', 'uses'=>'AdminPostsController@post']);
+    Route::get('/admin', function() {
+        return view('admin.index');
+    });
 });
